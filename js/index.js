b607949483d0f89875510a0f69dfe0996805872f
Richard <richard@richards-imac.home>
2018-12-17 17:45:11 +0000

    Changed scale line 37

--- js/index.js
+++ js/index.js
@@ -34,7 +34,7 @@ function init() {
       box.position.x = x;
       box.position.z = y;
       box.scale.y = 0.5;
-
+// changed box scale RBS line 37
       //box.rotation.x = Math.random() * 2 * Math.PI;;
       //box.rotation.y = Math.random() * 2 * Math.PI;;
       //box.rotation.z = Math.random() * 2 * Math.PI;;

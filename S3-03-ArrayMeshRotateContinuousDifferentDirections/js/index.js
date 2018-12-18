e93c943225a2645cca53161387a2828be7de45b8
alienindustry <richardbs@hotmail.co.uk>
2018-12-18 22:31:51 +0000

    new at line 36

--- S3-03-ArrayMeshRotateContinuousDifferentDirections/js/index.js
+++ S3-03-ArrayMeshRotateContinuousDifferentDirections/js/index.js
@@ -7,7 +7,7 @@ function init() {
   var W = window.innerWidth,
       H = window.innerHeight;
 
-  camera = new THREE.PerspectiveCamera(45, W / H, .1, 1000);
+  camera = new THREE.PerspectiveCamera(50, W / H, .1, 1000);//this changes the size of blocks
   camera.position.set(0, 55, 85);
   camera.lookAt(scene.position);
 
@@ -22,9 +22,9 @@ function init() {
   //renderer.shadowMapEnabled = true;
 
   //Create a two dimensional grid of objects, and position them accordingly
-  for (var x = -45; x <= 45; x += 5) { // Start from -45 and sequentially add one every 5 pixels
+  for (var x = -45; x <= 45; x += 3) { // Start from -45 and sequentially add one every 5 pixels
     for (var y = -30; y <= 30; y += 5) {
-      var boxGeometry = new THREE.BoxGeometry(3, 6, 3);
+      var boxGeometry = new THREE.BoxGeometry(3, 3, 3);  // was 3 6 3
       //The color of the material is assigned a random color
       var boxMaterial = new THREE.MeshLambertMaterial({color: Math.random() * 0xFFFFFF});
       var box = new THREE.Mesh(boxGeometry, boxMaterial);
@@ -33,7 +33,7 @@ function init() {
 
       box.position.x = x;
       box.position.z = y;
-      box.scale.y = 1.5;//lengthened the size of bricks
+      box.scale.y = 5.5;//lengthened the size of bricks RBS
 
       //box.rotation.x = Math.random() * 2 * Math.PI;;
       //box.rotation.y = Math.random() * 2 * Math.PI;;
@@ -48,7 +48,7 @@ function init() {
 }
 
 //Create two variables to store the direction of the motion
-var rot1 = 0;
+var rot1 = 1;// set rotation RBS
 var rot2 = 0;
 
 function drawFrame(){

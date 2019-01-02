f36656a95404b525e321b122519f53244b927e4b
alienindustry <richardbs@hotmail.co.uk>
2019-01-02 20:17:11 +0000

    Tried to load a code that would change the boxes to three colours but did not work

--- S3-03-ArrayMeshRotateContinuousDifferentDirections/js/index.js
+++ S3-03-ArrayMeshRotateContinuousDifferentDirections/js/index.js
@@ -22,26 +22,26 @@ function init() {
   //renderer.shadowMapEnabled = true;
 
   //Create a two dimensional grid of objects, and position them accordingly
-  for (var x = -45; x <= 45; x += 3) { // Start from -45 and sequentially add one every 5 pixels
-    for (var y = -30; y <= 30; y += 5) {//changing the y to a smaller no creates oblongs rather than blocks RBS
-      var boxGeometry = new THREE.BoxGeometry(3, 3, 3);  // was 3 6 3
-      //The color of the material is assigned a random color
-      var boxMaterial = new THREE.MeshLambertMaterial({color: Math.random() * 0xFFFFFF});
-      var box = new THREE.Mesh(boxGeometry, boxMaterial);
+  for (var x = -45; x <= 45; x += 5) { // Start from -45 and sequentially add one every 5 pixels
+    for (var y = -30; y <= 30; y += 5) {
+      var boxGeometry = new THREE.BoxGeometry(3, 6, 3);
+      //Random number generation - *3 for three variations
+      var randomNumber = Math.floor(Math.random()*3);
+      //Assign material based on random number
+      switch (randomNumber)
+      {
+        case 0:
+          var boxMaterial = new THREE.MeshLambertMaterial({color: 0xFF0000});
+          break;
+        case 1:
+          var boxMaterial = new THREE.MeshLambertMaterial({color: 0x00FF00});
+          break;
+        case 2:
+          var boxMaterial = new THREE.MeshLambertMaterial({color: 0x0000FF});
+          break;
+        break;
 
-      //box.castShadow = true;
-
-      box.position.x = x;
-      box.position.z = y;
-      box.scale.y = 5.5;//lengthened the size of bricks RBS
-
-      //box.rotation.x = Math.random() * 2 * Math.PI;;
-      //box.rotation.y = Math.random() * 2 * Math.PI;;
-      //box.rotation.z = Math.random() * 2 * Math.PI;;
-
-      scene.add(box);
-      cubes.push(box); //When we have our box object ready, add it to the array using push()
-    }
+      }
   }
 
   document.body.appendChild(renderer.domElement);

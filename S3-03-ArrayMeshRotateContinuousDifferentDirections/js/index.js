661dcbdbf112e290b85872b0f267694183e897df
alienindustry <richardbs@hotmail.co.uk>
2019-01-09 17:24:42 +0000

    tried adding an audio file at line 13 to 27 but had no effect

--- S3-03-ArrayMeshRotateContinuousDifferentDirections/js/index.js
+++ S3-03-ArrayMeshRotateContinuousDifferentDirections/js/index.js
@@ -7,49 +7,67 @@ function init() {
   var W = window.innerWidth,
       H = window.innerHeight;
 
-  camera = new THREE.PerspectiveCamera(40, W / H, .1, 1000);//this changes the size of blocks to large blocks RBS
+  camera = new THREE.PerspectiveCamera(40, W / H, .1, 1000);//this changes the size of blocks to large blocks RBS changed again
   camera.position.set(0, 110, 0);// changed camera angle from 0,55,85 so the wall is vertical RBS
   camera.lookAt(scene.position);
+  // create an AudioListener and add it to the camera
+  var listener = new THREE.AudioListener();
+  camera.add( listener );
 
+  // create a global audio source
+  var sound = new THREE.Audio( listener );
+
+  // load a sound and set it as the Audio object's buffer
+  var audioLoader = new THREE.AudioLoader();
+  audioLoader.load( 'sounds/ambient.ogg', function( buffer ) {
+  	sound.setBuffer( buffer );
+  	sound.setLoop( true );
+  	sound.setVolume( 0.5 );
+  	sound.play();
+  });
   var spotLight = new THREE.SpotLight(0xFFFFFF);
   spotLight.position.set(0, 1000, 0);
   scene.add(spotLight);
   //spotLight.castShadow = true;
 
   renderer = new THREE.WebGLRenderer({antialias:true});
-  renderer.setClearColor(0x17293a);
+  renderer.setClearColor(0x1db93f);// changed back ground colour to magenta line 20 RBS and to Lime green
   renderer.setSize(W, H);
   //renderer.shadowMapEnabled = true;
 
-  //Create a two dimensional grid of objects, and position them accordingly
-  for (var x = -45; x <= 45; x += 5) { // Start from -45 and sequentially add one every 5 pixels
-    for (var y = -30; y <= 30; y += 5) {
-      var boxGeometry = new THREE.BoxGeometry(3, 6, 3);
-      //Random number generation - *3 for three variations
-      var randomNumber = Math.floor(Math.random()*3);
-      //Assign material based on random number
-      switch (randomNumber)
-      {
-        case 0:
-          var boxMaterial = new THREE.MeshLambertMaterial({color: 0xFF0000});
-          break;
-        case 1:
-          var boxMaterial = new THREE.MeshLambertMaterial({color: 0x00FF00});
-          break;
-        case 2:
-          var boxMaterial = new THREE.MeshLambertMaterial({color: 0x0000FF});
-          break;
-        break;//loaded the above code from from 24-44 to change to 3 colours but had no effect RBS
+   //Create a two dimensional grid of objects, and position them accordingly
+  for (var x = -45; x <= 45; x += 2) { // Start from -45 and sequentially add one every 5 pixels
+    for (var y = -30; y <= 30; y += 2) {//changing the y to a smaller no creates oblongs rather than blocks RBS changing to 2 gives an interesting tiled effect
+      var boxGeometry = new THREE.BoxGeometry(3, 1, 3);  // was 3 6 3
+      //The color of the material is assigned a random color
+      var boxMaterial = new THREE.MeshLambertMaterial({color: Math.random()* 0xFFFFFF});
+      var box = new THREE.Mesh(boxGeometry, boxMaterial);
 
-      }
-  }
+      //box.castShadow = true;
+
+      box.position.x = x;
+      box.position.z = y;
+      box.scale.y = 100.5;// in creases and Decreases the size of bricks length RBS
+
+      //box.rotation.x = Math.random() * 2 * Math.PI;;
+      //box.rotation.y = Math.random() * 2 * Math.PI;;
+      //box.rotation.z = Math.random() * 2 * Math.PI;;
+
+      scene.add(box);
+      cubes.push(box); //When we have our box object ready, add it to the array using push()
+  44  }
+
+
+
+
+  }// changed code back
 
   document.body.appendChild(renderer.domElement);
 }
 
 //Create two variables to store the direction of the motion
-var rot1 = 1;// set rotation RBS
-var rot2 = 0;
+var rot1 = 10;// set rotation RBS
+var rot2 = 10;
 
 function drawFrame(){
   requestAnimationFrame(drawFrame);
@@ -63,7 +81,7 @@ function drawFrame(){
     } else {
       c.rotation.x = rot2;
     }
-  });
+  });///
 
   renderer.render(scene, camera);
 }
